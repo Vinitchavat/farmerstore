b0234af4793c5b07689f461a478447f3ce22fff4
TAAAE <athkrit_tae@hotmail.com>
2018-11-17 13:16:15 +0700

    add Preorder

---
+++ app/src/main/java/com/kongla/storeapp/CustomAdapterForPreorder.java
@@ -0,0 +1,116 @@
+package com.kongla.storeapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class CustomAdapterForPreorder extends BaseAdapter {
+    Context mContext;
+    ArrayList<String> clubkey = new ArrayList<String>();
+
+    public DatabaseReference callPre, callall;
+    GetData getData;
+    ArrayList<String> fruitName = new ArrayList<String>();
+
+    public CustomAdapterForPreorder(Context context, ArrayList<String> clubkey){
+        this.mContext = context;
+        this.clubkey = clubkey;
+    }
+    @Override
+    public int getCount() {
+        return clubkey.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, final View convertView, ViewGroup parent) {
+        final View view;
+        LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = mInflater.inflate(R.layout.listday, parent, false);
+
+        final TextView date = (TextView)view.findViewById(R.id.text1);
+        date.setText(""+clubkey.get(position));
+
+        ImageView img1 = view.findViewById(R.id.img1);
+        ImageView img2 = view.findViewById(R.id.img2);
+        ImageView img3 = view.findViewById(R.id.img3);
+        ImageView img4 = view.findViewById(R.id.img4);
+        ImageView img5 = view.findViewById(R.id.img5);
+        ImageView img6 = view.findViewById(R.id.img6);
+
+        final ImageView[] lay = {img1,img2,img3,img4,img5,img6};
+        final int[] fruit = {R.drawable.mangosteen, R.drawable.longan, R.drawable.orange, R.drawable.pineapple, R.drawable.rambutan, R.drawable.durian};
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        callPre = database.getReference().child("product").child("preorderProduct").child(clubkey.get(position));
+        callPre.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                fruitName.clear();
+                int countxx = 0;
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    getData = d.getValue(GetData.class);
+                    fruitName.add(getData.getFruitName());
+                }
+                for(int count = 0 ; count<fruitName.size();count++){
+                    for(int countIn = count+1 ; countIn<fruitName.size();countIn++){
+                        if(fruitName.get(count).matches(fruitName.get(countIn))){
+                            fruitName.remove(countIn);
+                        }
+                        else {
+                            countxx++;
+                        }
+                    }
+                }
+                for(int count = 0 ; count<fruitName.size();count++){
+                    if(fruitName.get(count).matches("ทุเรียน")){
+                        lay[count].setImageResource(fruit[5]);
+                    }
+                    else if(fruitName.get(count).matches("ลำไย")){
+                        lay[count].setImageResource(fruit[1]);
+                    }
+                    else if(fruitName.get(count).matches("แตงโม")){
+                        lay[count].setImageResource(fruit[3]);
+                    }
+                    else if(fruitName.get(count).matches("มังคุด")){
+                        lay[count].setImageResource(fruit[0]);
+                    }
+                    else if(fruitName.get(count).matches("เงาะ")){
+                        lay[count].setImageResource(fruit[4]);
+                    }
+                    else {
+                        lay[count].setImageResource(fruit[2]);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    return view;
+    }
+}
---
+++ app/src/main/java/com/kongla/storeapp/PreMain.java
@@ -0,0 +1,80 @@
+package com.kongla.storeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class PreMain extends AppCompatActivity {
+    public DatabaseReference callPre, callall;
+    ArrayList<String> fruitID = new ArrayList<String>();
+    ArrayList<String> fruitName = new ArrayList<String>();
+    ArrayList<Integer> price = new ArrayList<Integer>();
+    ArrayList<String> productName = new ArrayList<String>();
+    ArrayList<Integer> quantity = new ArrayList<Integer>();
+    ArrayList<String> unitPro = new ArrayList<String>();
+    CustomAdapterForPreorder adapterCus;
+    ArrayList<String> clubkey = new ArrayList<String>();
+    String getK;
+    int countKey, countDurian, countLongan, countOrange, countPineapple, countrambutan, countmangosteen;
+    ArrayList<Integer> countDurianA = new ArrayList<Integer>();
+    ArrayList<Integer> countLonganA = new ArrayList<Integer>();
+    ArrayList<Integer> countOrangeA = new ArrayList<Integer>();
+    ArrayList<Integer> countPineappleA = new ArrayList<Integer>();
+    ArrayList<Integer> countrambutanA = new ArrayList<Integer>();
+    ArrayList<Integer> countmangosteenA = new ArrayList<Integer>();
+    ArrayList<String[]> fruitcount = new ArrayList<String[]>();
+    public int i;
+    String[] day;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.premain);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        callPre = database.getReference().child("product").child("preorderProduct");
+        callPre.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                countKey = 0;
+                clubkey.clear();
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    getK = d.getKey();
+                    clubkey.add(getK);
+                    countKey = countKey + 1;
+                }
+                String[] mStringArray = new String[clubkey.size()];
+                mStringArray = clubkey.toArray(mStringArray);
+                final String[] finalMStringArray = mStringArray;
+                ListView list = (ListView) findViewById(R.id.list);
+                adapterCus =new CustomAdapterForPreorder(getApplicationContext(),clubkey);
+                list.setAdapter(adapterCus);
+
+                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent next = new Intent(PreMain.this, PreListDay.class);
+                        next.putExtra("DayPre", finalMStringArray[position]);
+                        startActivity(next);
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+    }
+}
