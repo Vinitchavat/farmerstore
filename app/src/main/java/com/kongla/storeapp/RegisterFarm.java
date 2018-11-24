1262477f6df241a306e78f5d4d2eb6f3fe81e393
TAAAE <athkrit_tae@hotmail.com>
2018-11-24 14:29:24 +0700

    add pic in RegisterFarm

--- app/src/main/java/com/kongla/storeapp/RegisterFarm.java
+++ app/src/main/java/com/kongla/storeapp/RegisterFarm.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.util.UUID;
+
+import static com.kongla.storeapp.chatNew.PICK_IMAGE;
+
 public class RegisterFarm extends AppCompatActivity {
 
     private EditText editName, editDes, editNum, editAdd;
@@ -27,6 +34,8 @@ public class RegisterFarm extends AppCompatActivity {
     SharedPreferences sp;
     SharedPreferences.Editor editor;
     FirebaseAuth firebaseAuth;
+    String ID;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,17 @@ public class RegisterFarm extends AppCompatActivity {
         editNum = (EditText) findViewById(R.id.editTextFarmNum);
         editAdd = (EditText) findViewById(R.id.editTextFarmAdd);
         loadingBar = new ProgressDialog(RegisterFarm.this);
+        Button choosePic = findViewById(R.id.choosePic);
+        choosePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent
+                        , "Select Picture"), PICK_IMAGE);
+                ID = UUID.randomUUID().toString();
+            }
+        });
 
         String mes = i.getStringExtra("txt");
         if (mes.matches("not")) {
@@ -56,6 +76,9 @@ public class RegisterFarm extends AppCompatActivity {
                 String fAdd = editAdd.getText().toString().trim();
                 if (TextUtils.isEmpty(fName) || TextUtils.isEmpty(fDes) || TextUtils.isEmpty(fNum) || TextUtils.isEmpty(fAdd)) {
                     Toast.makeText(getApplicationContext(), R.string.register_empty_warning, Toast.LENGTH_LONG).show();
+                }
+                else if (uri==null){
+                    Toast.makeText(getApplicationContext(), "กรุณาใส่ภาพ", Toast.LENGTH_LONG).show();
                 }
                 else if (fNum.length() != 13) {
                     Toast.makeText(getApplicationContext(), "กรุณาใส่หมายเลขฟาร์มให้ครบ", Toast.LENGTH_LONG).show();
@@ -134,5 +157,17 @@ public class RegisterFarm extends AppCompatActivity {
                 doubleBackToExitPressedOnce = false;
             }
         }, 2000);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            uri = data.getData();
+            File f = new File("" + uri);
+
+            String pic =  f.getName();
+            TextView PicName = (TextView)findViewById(R.id.picName);
+            PicName.setText(pic);
+        }
     }
 }
